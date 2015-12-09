0196a0b1099fdcf59f2b46e55c6fe2bf165fcb2c
Jose Manuel Chavez den la Cruz <jmchdlc@Mac-mini-de-Laslo.local>
2015-12-08 21:37:06 -0600

    fdfd

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ app.get('/', function (req, res) {
 });
 
 io.on('connection', function (socket) {
-	console.log(socket);
-	socket.emit('news', { hello: 'world' });
+	io.emit('news', { hello: 'world' });
 	socket.on('my other event', function (data) {
 		console.log(data);
 	});
