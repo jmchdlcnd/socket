76a1007b285aa9f2faacd15be8e5db7b847b017c
Jose Manuel Chavez den la Cruz <jmchdlc@Mac-mini-de-Laslo.local>
2015-12-09 11:19:57 -0600

    rew

--- app.js
+++ app.js
@@ -8,26 +8,18 @@ var io = require('socket.io')(server);
 server.listen(1024);
 
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 })
 
 var ns1 = io.of('/ns1');
-var ns2 = io.of('/ns2');
+//var ns2 = io.of('/ns2');
 
 ns1.on('connection', function (socket) {
 	//ns1.emit('news', { hello: 'soy el 1' });
 
-	io.emit('news', {hello: 'conexion en el 1'});
+	socket.emit('news', {hello: 'Ti id es: ' + socket.id});
 
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
-});
-
-ns2.on('connection', function (socket) {
-	ns2.emit('news', { hello: 'soy el 2' });
-
-	socket.on('my other event', function (data) {
-		console.log(data);
-	});
+	/*socket.on('trip', function (name) {
+		console.log(name);
+	});*/
 });
