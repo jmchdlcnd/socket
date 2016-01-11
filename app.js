09303d207fba7d3244c7640c80fe35d0c22da5e4
Jose Manuel Chavez den la Cruz <jmchdlc@Mac-mini-de-Laslo.local>
2016-01-11 11:06:05 -0600

    testing

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ d.on('connection', function (socket) {
 	  			console.log(socketId);
 
 	  			//io.to(socketId).emit('driver/chance', new Date);
-	  			ioEmitter.of('/dispatch').emit('driver/chance', new Date);
+	  			ioEmitter.of('/dispatch').sockets[socketId].emit('driver/chance', new Date);
 	  		});
 	  	});
 	});
